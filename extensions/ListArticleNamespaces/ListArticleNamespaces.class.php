ae9674b0241e685d83b58bc2c487ae38e14d9de2
sergey <sergey.menshikov@gmail.com>
2018-08-20 19:52:37 -0700

    hide non-whitelisted namespaces from the list of available article namespaces on a page

--- extensions/ListArticleNamespaces/ListArticleNamespaces.class.php
+++ extensions/ListArticleNamespaces/ListArticleNamespaces.class.php
@@ -17,7 +17,8 @@ class ListArticleNamespaces {
 
 	// render tag
 	public static function renderTag( $input, array $args, Parser $parser, PPFrame $frame ) {
-		global $wgExtraNamespaces;
+		global $wgExtraNamespaces, $wgUser, $wgWhitelistReadRegexp;
+		$loggedIn = $wgUser->isSafeToLoad() && $wgUser->isLoggedIn();
 		$title = $parser->getTitle();
 		$titleText = $title->getText(); // without namespace
 		$nsText = $title->getNsText();
@@ -27,6 +28,18 @@ class ListArticleNamespaces {
 			$possibleTitle = Title::newFromText($namespaceName.':'.$titleText);	
 			if($possibleTitle->exists()){
 				$url = $possibleTitle->getFullUrl();
+				// do not show non-whitelisted URLs
+				if(!$loggedIn){ 
+					$fullText = $possibleTitle->getFullText();
+					$whitelisted = false;
+					foreach ( $wgWhitelistReadRegexp as $listItem ) {
+						if ( preg_match( $listItem, $fullText ) ) {
+							$whitelisted = true;
+							break;
+						}
+					}
+					if(!$whitelisted) continue;
+				}
 				if($namespaceName == $nsText)
 					$output .= "&bull;&nbsp;<span class=\"list-article-namespaces-item selected\">$namespaceName</span> ";
 				else
